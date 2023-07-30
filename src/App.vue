c17af8aa378fc2292877776d580a1550a4660438
ChenR-Y <10817154+chenr-y@user.noreply.gitee.com>
2023-07-30 16:55:26 +0800

    11

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-row>
+    <a-col span='16'>
+      <img alt="Vue logo" src="./assets/wmh.jpg" :style="{ width: '300px' }">
+    </a-col>
+    <a-col span='16' :class=logotip>
+      from that man
+    </a-col>
+  </a-row>    
+</template>
+
+<script>
+
+alert("投产！！!");
+</script>
+
+<style>
+.logotip{
+  font-size: 0px;
+}
+</style>
