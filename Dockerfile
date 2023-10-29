9ce87d2824052adcb93e4e3648a7d01dddc61e40
ChenR-Y <10817154+chenr-y@user.noreply.gitee.com>
2023-10-29 20:53:34 +0800

    11

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,19 @@
 
-FROM nginx:1.21.0-alpine
-LABEL Author teddy
+#基础镜像
+FROM nginx:latest
+
+#设置工作路径
+#WORKDIR /app 
+
+#作者声明
+#LABEL Author teddy
+
+#复制应用程序代码到容器中
 COPY dist /usr/share/nginx/html
+
+#暴露端口
+#EXPOSE 80 
+
+##设置环境变量
+#ENV NODE_ENV=production
+
--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: [
-      '6da521b5.r1.vip.cpolar.cn', // 允许访问的域名地址，k
-      'www.cyberroc.work',
-      'http://120.79.254.225/' ,
-      '192.168.125.95:8080'
-      
-    ],
+    allowedHosts:'all',
   },
 })
